cf79c3ecaf4c07e28aa2174f75111960f412e858
Saidmurod <saidmurod98developer@gmail.com>
2024-11-23 13:57:18 +0500

    fix bug

--- app/src/main/java/us/smt/myfinance/ui/screen/tab/MainTabScreen.kt
+++ app/src/main/java/us/smt/myfinance/ui/screen/tab/MainTabScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
